300365525839611704c87c810372ba2b4b4007ce
Taylor Barnabic <tbarnabic1@gmail.com>
2021-06-15 14:15:37 -0400

    Solved 02

--- problems/02-sum-array.js
+++ problems/02-sum-array.js
@@ -10,8 +10,20 @@ sumArray([0, 1, -3]); //  -2
 sumArray([1, 2, 3, 4, 5]); // 15
 ***********************************************************************/
 
-// your code here
-  
+function sumArray(arr) {
+  if (arr.length === 0) {
+    return arr;
+  }
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  return arr[0] + sumArray(arr.slice(1));
+}
+
+console.log(sumArray([1, 2, 3])); //  6
+console.log(sumArray([0, 1, -3])); //  -2
+console.log(sumArray([1, 2, 3, 4, 5])); // 15
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumArray;
