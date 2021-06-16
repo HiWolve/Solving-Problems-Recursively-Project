519d209cce8d358a7ab19be0af907fea0434f5f0
Kreston <kreston.cm@gmail.com>
2021-06-16 15:49:25 -0500

    #8 complete

--- problems/08-advanced-exponent.js
+++ problems/08-advanced-exponent.js
@@ -2,9 +2,27 @@
 Write a more advanced version of the recursive `exponent` function that
 you just wrote. Instead of multiplying the base number by itself n power of
 times, like you did previously, you will be squaring the results of the base
-number raised to the power of half of n power.
+number raised to the power of half of n power.*/
 
-The following is math, not JavaScript:
+function advancedExponent(num, power) {
+  if (power === 1) {
+    return num;
+  }
+
+  if (power === 0) {
+    return 1;
+
+  }
+  if (power % 2 === 0) {
+    return advancedExponent(num, power / 2) ** 2
+  }
+  else {
+    return num * (advancedExponent(num, (power - 1) / 2) ** 2)
+  }
+
+}
+
+/*The following is math, not JavaScript:
 
 exponent(b, 0) // 1
 exponent(b, 1) // b
@@ -18,31 +36,26 @@ Remember that you don't need to do anything special to square a number, just
 calculate the value and multiply it by itself. So don't cheat and use
 exponentiation in your solution.
 
-Examples:
+Examples:*/
 
-advancedExponent(2, 0); // 1
-advancedExponent(2, 1); // 2
-advancedExponent(2, 2); // 4
-advancedExponent(2, 3); // 8
-advancedExponent(2, 4); // 16
-advancedExponent(2, 5); // 32
-advancedExponent(2, 6); // 64
-advancedExponent(2, 7); // 128
-advancedExponent(2, 8); // 256
-advancedExponent(2, 9); // 512
-advancedExponent(2, 10); // 1024
-advancedExponent(2, 11); // 2048
-advancedExponent(2, 12); // 4096
+console.log(advancedExponent(2, 0)); // 1
+console.log(advancedExponent(2, 1)); // 2
+console.log(advancedExponent(2, 2)); // 4
+console.log(advancedExponent(2, 3)); // 8
+console.log(advancedExponent(2, 4)); // 16
+console.log(advancedExponent(2, 5)); // 32
+console.log(advancedExponent(2, 6)); // 64
+console.log(advancedExponent(2, 7)); // 128
+console.log(advancedExponent(2, 8)); // 256
+console.log(advancedExponent(2, 9)); // 512
+console.log(advancedExponent(2, 10)); // 1024
+console.log(advancedExponent(2, 11)); // 2048
+console.log(advancedExponent(2, 12)); // 4096
 
-For each of the examples above, figure out how many times your code should
+/*For each of the examples above, figure out how many times your code should
 be recursively calling `advancedExponent`. Find a way to visually see how many
 times `advancedExponent` is being recursively called.
 ***********************************************************************/
-
-
-function advancedExponent(b, n) {
-  // your code here
-}
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
